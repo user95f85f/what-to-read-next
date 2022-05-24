d1f7210e647e046d8d036e39b71ff664fc6965ee
user95f85f <48297795+user95f85f@users.noreply.github.com>
2022-05-24 19:19:56 +0000

    Add files via upload

---
+++ share/readme-NOW/get-documentation-cmd.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use List::Util qw/shuffle/;
+
+my $number = $ARGV[0];
+my $local_counter = 0;
+for(<*.txt>){
+  my $filename=$_;
+  open(my ${f}, "<$filename") or die $!;
+  while(<$f>){
+    chomp;
+    if($local_counter == $number){
+      my @a = split /\//;
+      my @b = shuffle(@a);
+      my $target_function_module_cmd_whatever = $b[0];
+      if($filename eq 'perl.txt'){
+        print "perldoc -f $target_function_module_cmd_whatever\n";
+      }
+      elsif($filename eq 'perl-modules.txt'){
+        print "perldoc $target_function_module_cmd_whatever\n";
+      }
+      elsif($filename eq 'bash.txt'){
+        print "help $target_function_module_cmd_whatever\n";
+      }
+      elsif($filename eq 'terminal.txt'){
+        print "man $target_function_module_cmd_whatever\n";
+      }
+      elsif($filename eq 'dpkg.txt'){
+        print "apt-cache show $target_function_module_cmd_whatever\n";
+      }
+      else{
+        die 'you fuck.';
+      }
+    }
+    $local_counter++;
+  }
+}
